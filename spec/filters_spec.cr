671c57d40490116a8c2989f64f612c8bd502108c
Arnaud Fernandés <arnaud.fernandes@laposte.net>
2016-12-15 08:23:36 +0100

    Abs filter enhancement

--- spec/filters_spec.cr
+++ spec/filters_spec.cr
@@ -5,11 +5,10 @@ include Liquid::Filters
 describe Liquid::Filters do
   describe Abs do
     it "filter var and return absolute value" do
-      filtered = Abs.filter -12
-      filtered.should eq 12
-
+      Abs.filter(-12).should eq 12
       Abs.filter(12).should eq 12
       Abs.filter("wrong").should eq "wrong"
+      Abs.filter("-21.25").should eq 21.25
     end
   end
 end
--- src/liquid/filters.cr
+++ src/liquid/filters.cr
@@ -20,12 +20,25 @@ module Liquid::Filters
     abstract def filter(data : Context::DataType) : Context::DataType
   end
 
+  # Filter abs
+  # 
+  # Returns the absolute value of a number.
+  #
+  # `{{ -17 | abs }}` => `17`
+  # 
+  # `{{ 4 | abs }}` => `4`
+  # 
+  # abs will also work on a string if the string only contains a number.
+  # 
+  # `{{ "-19.86" | abs }}` => `19.86`
   class Abs
     extend Filter
 
     def self.filter(data : Context::DataType) : Context::DataType
       if data.responds_to? :abs
         data.abs
+      elsif data.is_a? String && (float = data.to_f32?)
+        float.abs
       else
         data
       end
