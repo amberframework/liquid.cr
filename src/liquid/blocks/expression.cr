012dcee71bf8a4a00948f6e48633f09ee153afd3
Arnaud Fernandés <arnaud.fernandes@laposte.net>
2016-12-30 08:45:13 +0100

    Move regexp into separate file

--- src/liquid/blocks/expression.cr
+++ src/liquid/blocks/expression.cr
@@ -1,32 +1,9 @@
 require "./block"
 require "../filters"
 require "../context"
+require "../regex"
 
 module Liquid::Block
-  OPERATOR = /==|!=|<=|>=|<|>/
-  GVAR     = /[a-z]\w*(\.[a-z]\w*)*/
-  VAR      = /[a-z]\w*(?:\.[a-z]\w*)*/
-
-  STRING      = /"[^"]*"/
-  INT         = /-?[1-9][0-9]*/
-  FLOAT       = /#{INT}\.[0-9]+/
-  TYPE        = /(?:#{STRING})|(?:#{INT})|(?:#{FLOAT})/
-  TYPE_OR_VAR = /(?:#{TYPE})|(?:#{VAR})/
-  CMP         = /(?:#{TYPE_OR_VAR}) ?(?:#{OPERATOR}) ?(?:#{TYPE_OR_VAR})/
-  GCMP        = /(?<left>#{TYPE_OR_VAR}) ?(?<op>#{OPERATOR}) ?(?<right>#{TYPE_OR_VAR})/
-
-  GSTRING = /^"(?<str>[^"]*)"$/
-  GINT    = /(?<intval>#{INT})/
-  GFLOAT  = /(?<floatval>#{FLOAT})/
-
-  FILTER    = /#{VAR}(?:: ?#{TYPE_OR_VAR}(?:, ?#{TYPE_OR_VAR})*)?/
-  GFILTER   = /(?<filter>#{VAR})(?:: ?(?<args>#{TYPE_OR_VAR}(?:, ?#{TYPE_OR_VAR})*))?/
-  FILTERED  = /#{TYPE_OR_VAR}(?:\s?\|\s?#{FILTER})+/
-  GFILTERED = /(?<first>#{TYPE_OR_VAR})(\s?\|\s?(#{FILTER}))+/
-
-  EXPR = /\s*(?<left>#{VAR}) ?(?<op>#{OPERATOR}) ?(?<right>#{TYPE})\s*/
-
-  MULTIPLE_EXPR = /(?: (?<op>or|and) )?(?<expr>#{CMP})/
 
   abstract class AbstractExpression < Node
   end
