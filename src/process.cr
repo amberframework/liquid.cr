73c9b2dabb3be57502d9456a0f734fa8953523a8
Davor Ocelic <docelic@spinlocksolutions.com>
2018-02-06 09:39:20 +0100

    Support passing custom context when rendering via Kilt (#24)
    With this patch, support for rendering via Kilt is complete.
    
    If custom context is not provided, then behavior remains as it
    was before - a new context object is automatically created and
    object's instance variables are made visible in it.
    
    If custom context is provided, then that context is used.

--- src/process.cr
+++ src/process.cr
@@ -1,8 +1,8 @@
 require "json"
 require "./liquid"
 
-filename, io = ARGV[0], ARGV[1]
+filename, io, context = ARGV[0], ARGV[1], ARGV[2]?
 raise "Liquid template: #{filename} doesn't exist." unless File.exists?(filename)
 
 tpl = Liquid::Template.parse File.read(filename)
-tpl.to_code io, STDOUT
+tpl.to_code io, STDOUT, context
