8d2182fdff5c3e66897c64a3ca5292824f48384a
Brian J. Cardiff <bcardiff@gmail.com>
2019-07-30 09:23:17 -0300

    Fix Filters::Filter#filter signature
    Leftover from a67e4a3179e207edc8991c85b46df4c12a57b2f6

--- src/liquid/filters/base.cr
+++ src/liquid/filters/base.cr
@@ -1,5 +1,5 @@
 require "../context"
 
 module Liquid::Filters::Filter
-  abstract def filter(data : Context::DataType, arguments : Array(Context::DataType)?) : DataType
+  abstract def filter(data : Any, arguments : Array(Any)?) : Any
 end
