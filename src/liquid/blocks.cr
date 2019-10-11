dba7bd05221298f8d43c6c3d3e51d2f05f5e806f
Shootingfly <xiefei@yolanda.hk>
2019-10-11 11:16:32 +0800

    Fix Raw type error

--- src/liquid/blocks.cr
+++ src/liquid/blocks.cr
@@ -15,15 +15,15 @@ require "./blocks/raw"
 include Liquid::Block
 
 module Liquid
-  BlockRegister.register "if", If
-  BlockRegister.register "elsif", ElsIf, false
-  BlockRegister.register "else", Else, false
-  BlockRegister.register "for", For
-  BlockRegister.register "capture", Capture
-  BlockRegister.register "assign", Assign, false
-  BlockRegister.register "increment", Increment, false
-  BlockRegister.register "decrement", Decrement, false
-  BlockRegister.register "include", Include, false
-  BlockRegister.register "raw", Raw
-  BlockRegister.register "comment", Comment
+  BlockRegister.register "if", Liquid::Block::If
+  BlockRegister.register "elsif", Liquid::Block::ElsIf, false
+  BlockRegister.register "else", Liquid::Block::Else, false
+  BlockRegister.register "for", Liquid::Block::For
+  BlockRegister.register "capture", Liquid::Block::Capture
+  BlockRegister.register "assign", Liquid::Block::Assign, false
+  BlockRegister.register "increment", Liquid::Block::Increment, false
+  BlockRegister.register "decrement", Liquid::Block::Decrement, false
+  BlockRegister.register "include", Liquid::Block::Include, false
+  BlockRegister.register "raw", Liquid::Block::Raw
+  BlockRegister.register "comment", Liquid::Block::Comment
 end
