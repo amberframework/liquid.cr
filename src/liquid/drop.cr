f73cc716c9695cf12de06dd95a8ef220bd38a1ba
Hugo Parente Lima <hugo@acumera.com>
2023-01-10 18:50:43 -0300

    Add Liquid::Ignore anotation to ignore methods in Drop objects.

--- src/liquid/drop.cr
+++ src/liquid/drop.cr
@@ -1,6 +1,10 @@
 require "./any"
 
 module Liquid
+  # Drop methods with this annotation arent' exported to liquid.
+  annotation Ignore
+  end
+
   abstract class Drop
     macro inherited
       {% verbatim do %}
@@ -8,7 +12,7 @@ module Liquid
         def call(method : String) : Liquid::Any
           case method
           {% for method in @type.methods %}
-            {% if method.args.size == 0 && method.visibility == :public %}
+            {% if method.args.size == 0 && method.visibility == :public && !method.annotation(Liquid::Ignore) %}
               when {{ method.name.stringify }}
                 ret = {{ method.name.id }}
                 ret.is_a?(Liquid::Any) ? ret : Liquid::Any.new(ret)
