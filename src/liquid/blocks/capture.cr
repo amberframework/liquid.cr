2c16d82d161a19e10ac3183b2d90bfd613c9c6e4
Hugo Parente Lima <hugo@acumera.com>
2023-01-13 16:47:30 -0300

    Pass on two more golden tests related to capture tag.

--- src/liquid/blocks/capture.cr
+++ src/liquid/blocks/capture.cr
@@ -4,15 +4,7 @@ module Liquid::Block
   class Capture < BeginBlock
     getter var_name : String
 
-    def initialize(@var_name)
-    end
-
-    def initialize(content : String)
-      if content =~ VAR
-        @var_name = content
-      else
-        raise InvalidNode.new "capture block needs an argument"
-      end
+    def initialize(content @var_name)
     end
   end
 end
