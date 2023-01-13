9509073168163bf369d3eaae8c5baaf8c92da8d4
Hugo Parente Lima <hugo@acumera.com>
2023-01-13 15:05:28 -0300

    Remove unused regexes.

--- spec/regex_spec.cr
+++ spec/regex_spec.cr
@@ -22,42 +22,5 @@ module Liquid
       ok.each &.match(/^#{TYPE_OR_VAR}$/).should_not be_nil
       err.each &.match(/^#{TYPE_OR_VAR}$/).should be_nil
     end
-
-    it "should match a comparison" do
-      ok = ["a == !b", "!-a == ---b", "!--a == !!b", "sdf != fds", "dd < 12", "true > false", "hh==hh", "12 <= var"]
-      err = ["a = b", "dfs ! fff", "ddd=ddd", "12>"]
-      ok.each &.match(/^#{CMP}$/).should_not be_nil
-      err.each &.match(/^#{CMP}$/).should be_nil
-    end
-
-    it "should match filters" do
-      ok = ["a | b", "a | b | v", "-12 | abs", "\"toto\" | abs"]
-      err = ["a ||", "dfs |", "|ddd=ddd", "1|2", "dd | 12"]
-      ok.each &.match(/^#{GFILTERED}$/).should_not be_nil
-      err.each &.match(/^#{GFILTERED}$/).should be_nil
-    end
-
-    it "should match filters with multiple arguments" do
-      str = "filtered | filter: arg1, arg2, arg3"
-      matches = str.match(/^#{GFILTERED}$/).not_nil!
-      matches[0].should eq str
-      matches["first"].should eq "filtered"
-      matches["filter"].should eq "filter"
-      matches["args"].should eq "arg1, arg2, arg3"
-    end
-
-    it "should match filters with multiple arguments 2" do
-      str = "filtered | filter: \"a,b\", 'c, d', e"
-      matches = str.match(/^#{GFILTERED}$/).not_nil!
-      matches[0].should eq str
-      matches["first"].should eq "filtered"
-      matches["filter"].should eq "filter"
-      matches["args"].should eq "\"a,b\", 'c, d', e"
-    end
-
-    it "should match a string" do
-      "\"content\"".match(GSTRING).should_not be_nil
-      "'content'".match(GSTRING).should_not be_nil
-    end
   end
 end
--- src/liquid/regex.cr
+++ src/liquid/regex.cr
@@ -1,6 +1,4 @@
 module Liquid
-  OPERATOR = /==|!=|<=|>=|<|>|contains/
-
   DQSTRING = /"([^"]|\\")*"/
   SQSTRING = /'([^']|\\')*'/
   STRING   = /(?:#{DQSTRING})|(?:#{SQSTRING})/
@@ -14,22 +12,4 @@ module Liquid
   VAR = /([-!]*(?<varbasename>[A-Za-z_]\w*)(?:(?<property>\.[A-Za-z_]\w*)|(?:\[(?<index>(?:#{STRING})|(?:#{INT})|(?1))\]))*\??)/
 
   TYPE_OR_VAR = /(?:#{TYPE})|(?:#{VAR})/
-  CMP         = /(?:#{TYPE_OR_VAR})\s*(?:#{OPERATOR})\s*(?:#{TYPE_OR_VAR})/
-  GCMP        = /(?<left>#{TYPE_OR_VAR})\s*(?<op>#{OPERATOR})\s*(?<right>#{TYPE_OR_VAR})/
-
-  GSTRING = /^(?:"(?<str>[^"]*)")|(?:'(?<str>[^']*)')$/
-  GINT    = /(?<intval>#{INT})/
-  GFLOAT  = /(?<floatval>#{FLOAT})/
-
-  FILTER_ARG  = TYPE_OR_VAR
-  FILTER_ARGS = /#{FILTER_ARG}(?:,\s*#{FILTER_ARG})*/
-  GFILTER     = /(?<filter>#{VAR})(?::\s*(?<args>#{FILTER_ARGS}))?/
-  GFILTERED   = /(?<first>#{TYPE_OR_VAR})(\s*\|\s*(#{GFILTER}))+/
-
-  CMPEXPR  = /\s*?#{GCMP}\s*?/
-  BOOLEXPR = /\s*?!?(?<bool>#{BOOL})\s*?/
-  EXPR     = /(?:#{CMPEXPR})|(?:#{BOOLEXPR})|(?:#{VAR})/
-
-  BOOLOP        = /(?:\s+(?:or|and)\s+)|(?:\s*(?:&&|\|\|)\s*)/
-  MULTIPLE_EXPR = /(?:(?<boolop>(?:#{BOOLOP})))?(?<expr>#{EXPR})/
 end
