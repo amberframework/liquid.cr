0f9ca01d2995ee62a38506292b23e7f955043069
Hugo Parente Lima <hugo@acumera.com>
2023-01-19 17:26:17 -0300

    Fix false negative on compact test found after fixes on first filter.

--- src/liquid/filters/compact.cr
+++ src/liquid/filters/compact.cr
@@ -7,14 +7,15 @@ module Liquid::Filters
     def self.filter(data : Any, args : Array(Any), options : Hash(String, Any)) : Any
       raise FilterArgumentException.new("compact filter expects at most 1 argument.") if args.size > 1
 
-      array = data.as_a?
-      return data if array.nil?
+      raw_data = data.raw
+      return Any.new(nil) if raw_data.nil?
+      return Any{data} unless raw_data.is_a?(Enumerable(Any))
 
       key = args.first?
-      return Any.new(array.reject(&.raw.nil?)) if key.nil? || !key.as_s?
+      return Any.new(raw_data.reject(&.raw.nil?)) if key.nil? || !key.as_s?
 
       key = key.as_s
-      result = array.reject do |item|
+      result = raw_data.reject do |item|
         raw_item = item.raw
         if raw_item.is_a?(Drop) || raw_item.is_a?(Hash)
           raw_item[key].raw.nil?
