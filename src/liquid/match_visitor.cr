fecc6c215d35a4009bb81d8a0073bae40efb690f
Chris Larsen <clarsendev@protonmail.com>
2023-04-02 14:04:51 -0400

    Add Template#match

---
+++ src/liquid/match_visitor.cr
@@ -0,0 +1,59 @@
+require "./blocks"
+
+module Liquid
+  class MatchVisitor < Visitor
+    def visit(node : Node) : String
+      return node.children.map { |e| self.visit e }.join
+    end
+
+    def visit(node : Block::RawNode) : String
+      return node.content
+    end
+
+    def visit(node : ExpressionNode)
+      return ".*"
+    end
+
+    def visit(node : If) : String
+      child_nodes = node.children.map { |e| self.visit e }
+
+      if elsif_arr = node.elsif
+        elsif_arr.each do |alt|
+          child_nodes << self.visit alt
+        end
+      end
+
+      if else_arr = node.else
+        child_nodes << self.visit else_arr
+      else
+        child_nodes << ""
+      end
+
+      return "(#{child_nodes.join("|")})"
+    end
+
+    def visit(node : Case)
+      child_nodes = [] of String
+
+      if when_arr = node.when
+        when_arr.each do |when_node|
+          child_nodes << self.visit when_node
+        end
+      end
+
+      if else_arr = node.else
+        child_nodes << self.visit else_arr
+      else
+        child_nodes << ""
+      end
+
+      return "(#{child_nodes.join("|")})"
+    end
+
+    def visit(node : For)
+      child_nodes = node.children.map { |e| self.visit e }
+
+      return "(#{child_nodes.join})*"
+    end
+  end
+end
