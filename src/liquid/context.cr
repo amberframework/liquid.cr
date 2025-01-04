8a6a366ccfef3f6935d9713277d16a3d8f52e458
Chris Larsen <clarsendev@protonmail.com>
2025-01-03 19:32:22 -0500

    Improve documentation for Context and Any classes

--- src/liquid/context.cr
+++ src/liquid/context.cr
@@ -2,14 +2,19 @@ require "./any"
 require "./blank"
 
 module Liquid
+  # A `Context` object provides the variables used by the `Template` object. These variables are stored
+  # as key-value pairs of type `String`, `Liquid::Any`.
+  #
+  #
   class Context
     # Context error mode.
     enum ErrorMode
-      # Raise exceptions on any error but `UndefinedVariable`, that can be accessed later by `Context#errors`.
+      # Raises exceptions on any error other than `UndefinedVariable`, which are saved and can be
+      # accessed later through `Context#errors`.
       Strict
-      # Like `Lax` mode, but the errors can be accessed later by `Context#errors`.
+      # Similar to `Lax` mode, but the errors can be accessed later through `Context#errors`.
       Warn
-      # Do the best to render something without raising any exceptions.
+      # Attempts to render the template without raising any exceptions.
       Lax
     end
 
@@ -22,9 +27,10 @@ module Liquid
     protected getter filter_options = Hash(String, Any).new(Any.new)
 
     property error_mode : ErrorMode
-    # List of errors found when rendering using this context.
+    # Returns a list of errors found when rendering using this context.
     getter errors = Array(LiquidException).new
 
+    # Creates a new `Context` with the given *error_mode* and *data*.
     def initialize(@error_mode = :lax, @data = Hash(String, Any).new)
       add_builtins
     end
@@ -87,25 +93,29 @@ module Liquid
       get(var)
     end
 
-    # Fetch a variable from context and return nil if the variable isn't found.
+    # Returns the value for the variable given by *var*, or nil if the variable isn't found.
     #
     # This doesn't trigger any exceptions or store any errors if the variable doesn't exists.
     def []?(var : String) : Any?
       @data[var]?
     end
 
+    # Sets the value of *var* to the given *value*.
     def set(var : String, value : Any) : Any
       @data[var] = value
     end
 
+    # Sets the value for *var* to an instance of `Liquid::Any` generated from *value*.
     def set(var : String, value : Any::Type) : Any
       set(var, Any.new(value))
     end
 
+    # Alias for `#set`
     def []=(var, value : Any) : Any
       set(var, value)
     end
 
+    # Alias for `#set`
     def []=(var, value : Any::Type) : Any
       set(var, value)
     end
